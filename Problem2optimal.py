2e41c6f11c8be7e4492cae24245f83026d8b2ef2
rahul <rahulyamsani13@gmail.com>
2022-04-29 18:29:28 +0530

    initial commit

--- Problem2optimal.py
+++ Problem2optimal.py
@@ -12,4 +12,17 @@ for i in range(len(l)):
 print(s)
 
 
+l=list(map(int,input().split()))
+s=[1]*len(l)
+p=1
+for i in range(1,len(l)):
+        s[i]*=(p*l[i-1])
+        p*=l[i-1]
+p=1
+for i in range(len(l)-2,-1,-1):
+        s[i]=s[i]*p*l[i+1]
+        p*=l[i+1]
+print(s)
+
+
 
